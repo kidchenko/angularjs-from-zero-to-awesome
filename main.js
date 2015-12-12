b31df50a6e4a189348d0c248eb031e9e29a9465e
kidchenko <kidchenko@gmail.com>
2015-12-12 18:33:12 -0200

    Submitting a form using

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 var app = angular.module('minmax', []);
 
-app.controller('MinMaxCtrl', function ($scope) {
+app.controller('MinMaxCtrl', function ($scope, $http) {
   $scope.formModel = {};
   $scope.onSubmit = function () {
     console.log('Juca submited!');
     console.log($scope.formModel);
+    $http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel)
+        .then(function (success) {
+          console.log(':)')
+        }, function (error) {
+          console.log(':C')
+        });
   };
 });
