d671f4db70fcd0c1f249c6f40d1e79e2e0d6f9d1
kidchenko <kidchenko@gmail.com>
2015-12-13 21:41:49 -0200

    Form validation using Angular Auto Validate

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
-var app = angular.module('minmax', []);
+var app = angular.module('minmax', ['jcs-autoValidate']);
+
+app.run(function (defaultErrorMessageResolver) {
+		defaultErrorMessageResolver.getErrorMessages().then(function (errorMessages) {
+			errorMessages['tooYoung'] = 'You must be at least {0} years old to use this site';
+			errorMessages['tooOld'] = 'You must be max {0} years old to use this site';
+			errorMessages['badUsername'] = 'Username can only contain numbers and letters and _';
+		});
+	}
+);
+
+
 
 app.controller('MinMaxCtrl', function ($scope, $http) {
   $scope.formModel = {};
+
   $scope.onSubmit = function () {
-    console.log('Juca submited!');
-    console.log($scope.formModel);
-    $http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel)
-        .then(function (success) {
-          console.log(':)')
-        }, function (error) {
-          console.log(':C')
-        });
+      console.log('Juca submited!');
+      console.log($scope.formModel);
+      $http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel)
+          .then(function (success) {
+            console.log(':)')
+          }, function (error) {
+            console.log(':C')
+          });
+
   };
 });
